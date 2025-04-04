699e813baf0716a02fe29d4e0315134bdaa6352b
SkibidiToiletButE <ocowan@mybce.catholic.edu.au>
2025-04-05 08:31:29 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
 let Money = 0
 let MoneyPerClick = 1
+
 let UpgradeLevel = 0
 let UpgradeCost = 10
-let LevelMulti = 1
+let UpgradeMulti = 1
 
 let Rebirths = 0
 let RebirthCost = 10000
 
-if (localStorage) {
+let loop = true
 
+//Game saving
+while (loop === true) {
+    if (localStorage) {
+        localStorage.Money = setItem(Money, Money)
+        localStorage.MoneyPerClick = setItem(MoneyPerClick, MoneyPerClick)
+        
+        localStorage.UpgradeLevel = setItem(UpgradeLevel, UpgradeLevel)
+        localStorage.UpgradeCost = setItem(UpgradeCost, UpgradeCost)
+        
+        localStorage.UpgradeMulti = setItem(UpgradeMulti, UpgradeMulti)
+        localStorage.Rebirths = setItem(Rebirths, Rebirths)
+        localStorage.RebirthCost = setItem(RebirthCost, RebirthCost)
+    }
 }
-            
+
+//Game loading
+window.onload = function Load() {
+    if (localStorage) {
+        Money = getItem(Money)
+        MoneyPerClick = getItem(MoneyPerClick)
+        
+        UpgradeLevel = getItem(UpgradeLevel)
+        UpgradeCost = getItem(UpgradeCost)
+        
+        UpgradeMulti = getItem(UpgradeMulti)
+        Rebirths = getItem(Rebirths)
+        RebirthCost = getItem(RebirthCost)
+    }
+}
+           
+//Click function
 function Click() {
     Money += MoneyPerClick
     document.getElementById("MoneyDisplay").innerHTML = Money
     document.getElementById("MoneyPerClickDisplay").innerHTML = MoneyPerClick
 }
 
+//Upgrade function
 function Upgrade() {
     if (Money >= UpgradeCost) {
         Money -= UpgradeCost
-        UpgradeLevel += 1
+        UpgradeLevel += UpgradeMulti
         UpgradeCost *= 2 
         MoneyPerClick += UpgradeLevel
 
@@ -31,13 +62,16 @@ function Upgrade() {
     }
 }
 
+//Rebirth function
 function Rebirth() {
     if (Money >= RebirthCost) {
         UpgradeLevel = 0
         Money = 0
         UpgradeCost = 10
+        MoneyPerClick = 1
         
-        MoneyPerClick *= 2
+        UpgradeMulti *= 2
+        MoneyPerClick *= 2 ** (Rebirths + 1)
         Rebirths += 1
         RebirthCost *= 2
 
